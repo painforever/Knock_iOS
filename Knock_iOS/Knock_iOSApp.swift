98dafbc4b3a79ecd24b0dfc3907cd70c7f1954a4
painforever <bjslxxx@126.com>
2021-10-27 20:17:53 -0400

    Initial Commit

---
+++ Knock_iOS/Knock_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Knock_iOSApp.swift
+//  Knock_iOS
+//
+//  Created by yusong on 10/27/21.
+//
+
+import SwiftUI
+
+@main
+struct Knock_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
