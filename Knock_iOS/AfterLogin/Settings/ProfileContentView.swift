f00717758031e72fc48f7ad1705a8835e1010675
painforever <bjslxxx@126.com>
2022-01-18 00:19:47 -0500

    a

---
+++ Knock_iOS/AfterLogin/Settings/ProfileContentView.swift
@@ -0,0 +1,54 @@
+//
+//  ProfileContentView.swift
+//  Knock_iOS
+//
+//  Created by yusong on 1/17/22.
+//
+
+import SwiftUI
+import SDWebImageSwiftUI
+
+struct ProfileContentView: View {
+    var body: some View {
+        VStack { 
+            VStack {
+                VStack {
+                    AnimatedImage(url: URL(string: "https://media-exp1.licdn.com/dms/image/C5103AQG1h4gxBsRAeg/profile-displayphoto-shrink_200_200/0/1516871913633?e=1648080000&v=beta&t=9WOQ0cL5n3_zU-ECXx9UGsmVF76ZMPJmLqYFeBEu4ww")).resizable().frame(width: 150, height: 150).cornerRadius(75)
+                    Text("#username").bold()
+                }
+                HStack {
+                    Text("Total Meetings: \(45)")
+                    Spacer()
+                    HStack(spacing: 2) {
+                        Text("Rating: ")
+                        ForEach(1..<5, id: \.self) { number in
+                            Image(systemName: "star.fill").resizable().frame(width: 20, height: 20).foregroundColor(.yellow)
+                        }
+                    }
+                }.padding()
+                HStack {
+                    Text("Satisfaction: \(87)%")
+                    Spacer()
+                    Text("Total hours: \(341) hrs")
+                }.padding()
+            }
+            .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * 0.45)
+            .background(Color(Constants.themeColor))
+            .foregroundColor(.white)
+            
+            VStack {
+                HStack {
+                    Text("Top areas: Business")
+                }
+            }
+            .background(Color(.green))
+            .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * 0.55)
+        }.ignoresSafeArea()
+    }
+}
+
+struct ProfileContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileContentView()
+    }
+}
